52db851e2dcda09b9f02312929b78d46caf01067
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2022-11-16 19:54:05 -0300

    Start

---
+++ SportsStore/Startup.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; set; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddDbContext<StoreDbContext>( opts => {
+                opts.UseSqlServer( Configuration["ConnectionStrings:SportsStoreConnection"] );
+            } );
+
+            services.AddScoped<IStoreRepository, EFStoreRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();                
+            });
+        }
+    }
+}
